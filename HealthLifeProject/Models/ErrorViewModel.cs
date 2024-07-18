477400d82bb4efa3a9c23a03d3538abbb6660f74
Eduard <Eduard@DESKTOP-HH8M6J0>
2024-07-18 10:39:04 +0300

    Добавьте файлы проекта.

---
+++ HealthLifeProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HealthLifeProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
