46af3db5890b22e95492e2e1e40c112374de8131
Spon_zu55 <Spon_zu55@student.itstep.org>
2024-07-21 09:22:26 +0300

    added about, error controller, 404 page

---
+++ HealthLifeProject/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthLifeProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ HealthLifeProject/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthLifeProject.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Error", "PageNotFound");
+        }
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
